ed1483a56b806d41e984ca6d683b3b84c79a5bef
Tuneron <tunon2010@yandex.ua>
2020-07-27 18:23:47 +0300

    init

---
+++ Menu/MainMenu.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void StartNewGame()
+    {
+        SceneManager.LoadScene(string.Format("CharacterCustomization"));
+    }
+    public void OpenGalley()
+    {
+        SceneManager.LoadScene(string.Format("Gallery"));
+    }
+
+    public void OpenSettings()
+    {
+        SceneManager.LoadScene(string.Format("Settings"));
+    }
+
+    public void OpenLoad()
+    {
+        SceneManager.LoadScene(string.Format("LoadSave"));
+    }
+
+    public void OpenSave()
+    {
+        SceneManager.LoadScene(string.Format("SaveGame"));
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
